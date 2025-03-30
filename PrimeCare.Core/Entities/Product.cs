72ec8353b55509521bab177faa3952962608e5a2
abdulrahman <abdulrahmn398@outlook.com>
2025-03-30 15:28:56 +0200

    add constr an setup first entity to test migrations an data base

---
+++ PrimeCare.Core/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace PrimeCare.Core.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
---
+++ PrimeCare.Infrastructure/Data/PrimeCareContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeCare.Core.Entities;
+
+namespace PrimeCare.Infrastructure.Data;
+
+public class PrimeCareContext : DbContext
+{
+    public PrimeCareContext(DbContextOptions<PrimeCareContext> options)
+        : base(options)
+    {
+
+    }
+    public DbSet<Product> Products { get; set; }
+}
