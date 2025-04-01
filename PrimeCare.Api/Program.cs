d1e0848e39fea1a79840962b7d29400ea9197611
abdulrahman <abdulrahmn398@outlook.com>
2025-04-01 12:53:07 +0200

    Add product repository pattern implementation
    Introduce `IProductRepository` interface and its implementation in `ProductRepository`. Register the repository in `Program.cs` for dependency injection. This enhances data management and promotes better separation of concerns in the application.

--- PrimeCare.Api/Program.cs
+++ PrimeCare.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PrimeCare.Core.Interfaces;
 using PrimeCare.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<PrimeCareContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ PrimeCare.Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeCare.Core.Entities;
+using PrimeCare.Core.Interfaces;
+
+namespace PrimeCare.Infrastructure.Data;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly PrimeCareContext _context;
+
+    public ProductRepository(PrimeCareContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product> GetProductByIdAsync(int id)
+        => await _context.Products.FindAsync(id);
+
+    public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        => await _context.Products.ToListAsync();
+}
