79e74283d1c248fa32d6bcf915e5d614d2dec548
abdulrahman <abdulrahmn398@outlook.com>
2025-04-04 20:09:06 +0200

    Refactor API structure and add new features
    - Updated `ProductController` to inherit from `BaseApiController`, centralizing API configurations.
    - Reintroduced `ProductDto` with XML documentation and essential properties for data transfer.
    - Enhanced `MappingProfiles` to include mappings between `Product` and `ProductDto`, utilizing `ProductUrlResolver`.
    - Added `ProductUrlResolver` to resolve product picture URLs based on configuration.
    - Introduced `BuggyController` for simulating various HTTP responses.
    - Created `PrimeCare.Application.csproj` targeting `net8.0` with implicit usings and nullable reference types.
    - Updated solution file to include the new `PrimeCare.Application` project.

--- PrimeCare.Api/Controllers/ProductController.cs
+++ PrimeCare.Api/Controllers/ProductController.cs
@@ -7,9 +7,7 @@ using PrimeCare.Core.Specifications;
 
 namespace PrimeCare.Api.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class ProductController : ControllerBase
+public class ProductController : BaseApiController
 {
     private readonly IGenericRepository<Product> _productRepo;
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
