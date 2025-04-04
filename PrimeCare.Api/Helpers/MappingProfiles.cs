aff7008d9cf5f0ec094c1431163a1dfc54d1a351
abdulrahman <abdulrahmn398@outlook.com>
2025-04-04 14:15:23 +0200

    handel static content

--- PrimeCare.Api/Helpers/MappingProfiles.cs
+++ PrimeCare.Api/Helpers/MappingProfiles.cs
@@ -10,6 +10,7 @@ public class MappingProfiles : Profile
     {
         CreateMap<Product, ProductDto>()
             .ForMember(dest => dest.ProductBrand, opt => opt.MapFrom(src => src.ProductBrand.Name))
-            .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src => src.ProductType.Name));
+            .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src => src.ProductType.Name))
+            .ForMember(dest => dest.PictureUrl, opt => opt.MapFrom<ProductUrlResolver>());
     }
 }
---
+++ PrimeCare.Api/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using PrimeCare.Api.Dtos;
+using PrimeCare.Core.Entities;
+
+namespace PrimeCare.Api.Helpers;
+
+public class ProductUrlResolver : IValueResolver<Product, ProductDto, string>
+{
+    private readonly IConfiguration _configuration;
+
+    public ProductUrlResolver(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string Resolve(Product source, ProductDto destination,
+        string destMember, ResolutionContext context)
+    {
+        if (!string.IsNullOrEmpty(source.PictureUrl))
+            return _configuration["ApiUrl"] + source.PictureUrl;
+
+        return null!;
+    }
+}
