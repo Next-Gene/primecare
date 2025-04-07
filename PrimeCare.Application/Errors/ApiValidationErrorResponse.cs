64c2707e76d37eaae9d3bd1c5dfef73d924e7d93
abdulrahman <abdulrahmn398@outlook.com>
2025-04-07 13:33:24 +0200

    Add XML documentation and Errors property to ApiValidationErrorResponse
    This commit introduces XML documentation comments for the
    `ApiValidationErrorResponse` class and its constructor,
    enhancing code clarity. Additionally, a new property
    `Errors` of type `IEnumerable<string>` is added to store
    validation error messages, initialized to `null!`.

--- PrimeCare.Application/Errors/ApiValidationErrorResponse.cs
+++ PrimeCare.Application/Errors/ApiValidationErrorResponse.cs
@@ -1,10 +1,19 @@
 ﻿namespace PrimeCare.Application.Errors;
 
+/// <summary>
+/// Represents an API validation error response with a status code and a list of validation errors.
+/// </summary>
 public class ApiValidationErrorResponse : ApiResponse
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ApiValidationErrorResponse"/> class.
+    /// </summary>
     public ApiValidationErrorResponse() : base(400)
     {
     }
 
+    /// <summary>
+    /// Gets or sets the validation errors.
+    /// </summary>
     public IEnumerable<string> Errors { get; set; } = null!;
 }
