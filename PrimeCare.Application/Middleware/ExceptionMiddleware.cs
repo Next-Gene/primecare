4eea70d1d30a981760b558de7a30fa5fdb8b9e93
abdulrahman <abdulrahmn398@outlook.com>
2025-04-08 20:26:44 +0200

    Refactor exception handling for StackTrace access
    Updated the access method for the `StackTrace` property in the `ExceptionMiddleware` class to use the null-forgiving operator. This change eliminates the unnecessary call to `ToString()` since `StackTrace` is already a string, improving code clarity and correctness.

--- PrimeCare.Application/Middleware/ExceptionMiddleware.cs
+++ PrimeCare.Application/Middleware/ExceptionMiddleware.cs
@@ -32,8 +32,8 @@ public class ExceptionMiddleware
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             var response = _env.IsDevelopment()
-                ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
-                : new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString());
+                ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace!.ToString())
+                : new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace!.ToString());
 
             var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
