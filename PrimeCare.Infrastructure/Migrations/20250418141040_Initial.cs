07ca49458c442c2880dff7c299cfac951e3e4c9f
Seif Emam <seifmoataz27249@gmail.com>
2025-04-18 16:15:33 +0200

    Refactor Category entity and migration files
    - Updated the `Category` class to include new properties: `CreatedAt`, `UpdatedAt`, `Slug`, and `ImageUrl`.
    - Introduced `CategoryConfiguration` for entity configuration.
    - Removed old migration files and added new migration logic in `20250418141040_Initial.cs` and `20250418141040_Initial.Designer.cs`.
    - Overall restructuring of the database schema for better data management.

--- PrimeCare.Infrastructure/Migrations/20250418141040_Initial.cs
+++ PrimeCare.Infrastructure/Migrations/20250418141040_Initial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -16,7 +17,12 @@ namespace PrimeCare.Infrastructure.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Slug = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    Description = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
                 },
                 constraints: table =>
                 {
