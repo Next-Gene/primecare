3a425914455422b8601eab45233685f41bdd4a6d
abdulrahman <abdulrahmn398@outlook.com>
2025-05-01 17:15:19 +0300

    Add sorting functionality to category retrieval
    - Updated `GetCategories` in `CategoryController` to accept an optional `sort` parameter.
    - Modified `CategoryService` constructor parameter order for clarity.
    - Enhanced `GetAllAsync` method in `CategoryService` to support sorting.
    - Implemented sorting logic in `CategoryPhotoSepcification` based on the `sort` parameter.
    - Updated `ICategoryService` interface to reflect changes in `GetAllAsync` method signature.

--- src/PrimeCare.Application/Services/Interfaces/ICategoryService.cs
+++ src/PrimeCare.Application/Services/Interfaces/ICategoryService.cs
@@ -6,7 +6,7 @@ namespace PrimeCare.Application.Services.Interfaces;
 public interface ICategoryService
 {
     Task<CategoryDto> GetByIdAsync(int id);
-    Task<IReadOnlyList<CategoryDto>> GetAllAsync();
+    Task<IReadOnlyList<CategoryDto>> GetAllAsync(string? sort);
     Task<ServiceResponse> AddAsync(CreateCategoryDto entity);
     Task<ServiceResponse> UpdateAsync(UpdateCategoryDto entity);
     Task<ServiceResponse> DeleteAsync(int id);
