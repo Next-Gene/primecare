c8e496b2c7c356492d0417fddd12889e7cd56216
Seif Emam <seifmoataz27249@gmail.com>
2025-05-14 23:06:37 +0300

    Add Identity and JWT packages; implement Address model
    Updated project files to include new package references for ASP.NET Core Identity and JWT authentication. Added `Address` and `ApplicationUser` classes to enhance user management and address handling capabilities.

---
+++ src/PrimeCare.Core/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿namespace PrimeCare.Core.Entities
+{
+    public class Address
+    {
+
+        public int Id { get; set; }
+        public string
+
+            FirstName
+        { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string ZipCode { get; set; } = null!;
+
+        public string AppUserId { get; set; }
+
+        public ApplicationUser AppUser { get; set; }
+
+    }
+}
